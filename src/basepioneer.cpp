e6acc235756a895a21ebd98f63c1cda67b04ef2c
Unknown <leonardo.perdomo.social@outlook.com>
2017-04-26 01:27:18 -0300

    nada

--- src/basepioneer.cpp
+++ src/basepioneer.cpp
@@ -30,21 +30,35 @@ int main(int argc, char** argv)
 	robot.attachKeyHandler(&keyHandler);
 	cout << "ESC para sair" << endl;
 
-	int sensores[8];
+	int sensores[10];
 
+	//robot.setDeltaHeading(degrees);
+	robot.setRotVelMax(10);
 	robot.enableMotors();
 	while (Aria::getRunning()) {
 		robot.lock();
 
-		for (int i=0;i<8;i++) sensores[i]=(int)(robot.getSonarRange(i));
+		for (int i=0;i<10;i++) sensores[i]=(int)(robot.getSonarRange(i));
+		cout << "1" << " " << sensores[0];
+		cout << " 2" << " " << sensores[1];
+		cout << " 3" << " " << sensores[2];
+		cout << " 4" << " " << sensores[3];
+		cout << " 5" << " " << sensores[4];
+		cout << " 6" << " " << sensores[5];
+		cout << " 7" << " " << sensores[6];
+		cout << " 8" << " " << sensores[7];
+		cout << " 9" << " " << sensores[8];
+		cout << " 10" << " " << sensores[9] << endl;
 
-		if (sensores[3] < 2500 && sensores[5] > 2500) {
+		/*if (sensores[3] < 2500 && sensores[5] > 2500) {
 			robot.setVel2(500, 100);
 		} else if (sensores[3] < 2500 && sensores[0] > 2500) {
 			robot.setVel2(100, 500);
 		} else {
 			robot.setVel2(500, 500);
-		}
+		}*/
+
+
 		robot.unlock();
 		ArUtil::sleep(100);
 	}
